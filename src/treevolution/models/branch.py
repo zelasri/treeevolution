fed878ce3986cd2cdd31705fe8d39bc18e70686a
zakaria.elasri2000 <zakaria.elasri2000@gmail.com>
2022-10-16 21:41:18 +0200

    Sprint2_Task1

---
+++ src/treevolution/models/branch.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from treevolution.context.context import Context
+from dateutil.relativedelta import relativedelta, MO
+from treevolution.context.weather import Weather
+from treevolution.models.state import TreeState
+from treevolution.models.tree import Tree
+from treevolution.models.state import BranchState
+
+
+class Branch(ABC):
+    def __init__(self,birth,tree:Tree):
+        self._tree = tree
+        self._birth = birth
+        self._state = BranchState.EVOLVE
+        self._lenght = 0
+        self._height = 0
+        self._max_length =  None
+        self._density = 0
+        
+    @property
+    def tree(self):
+        return self._tree
+    
+    @property
+    def birth(self):
+        return self._birth
+    
+    @property
+    def state(self):
+        return self._state
+    
+    @property
+    def lenght(self):
+        return self._lenght
+    
+    @property
+    def max_length(self):
+        return self._max_length
+
+   
+    
+    @property
+    @abstractmethod
+    def health(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def width(self):
+        pass
+    
+    @abstractmethod
+    def evolve(self):
+        pass
+        
+        
+    
+
