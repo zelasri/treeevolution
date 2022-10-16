1f8f5907f6d300260f241e76f5defdcb6c7187ca
zakaria.elasri2000 <zakaria.elasri2000@gmail.com>
2022-10-16 16:02:29 +0200

    task4

--- src/treevolution/world.py
+++ src/treevolution/world.py
@@ -2,12 +2,14 @@
 Module which contains World class
 """
 from datetime import datetime, timedelta,date
+from treevolution.models.tree import Tree
 
 class World():
     """
     World class in order to represent the forest and simulate the evolution of trees
     """
     def __init__(self, height, width, start_date):
+        self._trees = []
         self._height = height
         self._width = width
         self._start_date = datetime.strptime(str(start_date), "%Y-%m-%d %H:%M:%S")
@@ -19,4 +21,9 @@ class World():
         
     def date(self):
         return date.today()
+
+    def append(self,tree:Tree):
+        self._trees.append(tree)
+        return self._trees
+    def step 
     
