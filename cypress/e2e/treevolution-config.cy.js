94057a2ae0beff1d634534b1f8c2b4c4c5437e42
zakaria.elasri2000 <zakaria.elasri2000@gmail.com>
2022-12-08 18:27:03 +0100

    InterfaceTest4

--- cypress/e2e/treevolution-config.cy.js
+++ cypress/e2e/treevolution-config.cy.js
@@ -9,19 +9,15 @@ afterEach(() => {
     })
 
 
-describe('Story 3', () => {
-        it(' date de fin de simulation inférieure à celle de début. Un message d’erreur peut-être vérifié', () => {
+describe('Story 4', () => {
+        it('  possible de valider une configuration n’ayant aucun type d’arbre sélectionné', () => {
         cy.visit('http://0.0.0.0:5000/config')
         cy.wait(500) // add some delay
-        cy.get('#inputStartDate').type('2022-11-13').should('have.value','2022-11-13')
-        cy.get('#inputEndDate').type('2022-11-04').should('have.value','2022-11-04')
         cy.get('#inputKindsTree').select('treevolution.models.trees.oak.Ash')
-        cy.get('#inputDaysNumber').invoke('val',3)
-        cy.get('#inputSeed').invoke('val',42)
         cy.get('button[type="submit"]').click() // apres click cy.wait
-        cy.wait(500)
-        cy.get('div[role="alert"] > div').contains('Invalid start date and end date.') 
-        })
-        })
+        cy.wait(500) 
+            })
+            })
+    
     
 
