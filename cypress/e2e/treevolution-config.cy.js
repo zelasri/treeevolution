b3861f32cdceffb133b4013b1cfb362b1ce143cc
zakaria.elasri2000 <zakaria.elasri2000@gmail.com>
2022-12-08 19:25:50 +0100

    story 10

--- cypress/e2e/treevolution-config.cy.js
+++ cypress/e2e/treevolution-config.cy.js
@@ -9,8 +9,8 @@ afterEach(() => {
     })
 
 
-describe('Story 9', () => {
-        it('Lorsqu’une simulation est créée depuis l’onglet /simulation à partir d’une configuration. Celle-ci peut-être supprimée si la configuration vient à changer.', () => {
+describe('Story 10', () => {
+        it('verification que le nombre des arbres evolue a la fin de la simulation', () => {
         cy.visit('http://0.0.0.0:5000/config')
         cy.get('#inputStartDate').type('2022-12-13')
         cy.get('#inputEndDate').type('2023-12-20')
@@ -18,16 +18,18 @@ describe('Story 9', () => {
         cy.get('input[type=range]').as('range').invoke('val',5)
         cy.get('@range').should('have.value', 5)
         cy.get('button[type="submit"]').click()
-        
         cy.visit('http://0.0.0.0:5000/simulation')
         cy.get('#initSimulationBtn').click()
+        cy.get('#legendPanel > table > tbody > tr:nth-child(2) > td:nth-child(2)')
+        .invoke('text')
+        .then((text1) => {
         cy.get('#runSimulationBtn').click()
-        cy.visit('http://0.0.0.0:5000/config')
-        cy.get('#inputStartDate').type('2022-12-15')
-        cy.get('button[type="submit"]').click()
-        cy.wait(500)
-        cy.visit('http://0.0.0.0:5000/simulation')
-        cy.get('#legendPanel > div > p > i').contains('No current simulation available')
-        
-        })
-})
+        cy.get('#simulationProgress',{timeout: 20000}).should('contain', '100%')
+        cy.get('#legendPanel > table > tbody > tr:nth-child(2) > td:nth-child(2)')
+        .invoke('text')
+        .should((text2) => {
+            expect(text1).not.eq(text2)
+            })
+            })
+            })
+            })
