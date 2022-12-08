e1d39e0182d43e0f13e7934ca89cf4347ed667bc
zakaria.elasri2000 <zakaria.elasri2000@gmail.com>
2022-12-08 19:17:45 +0100

    story 9

--- cypress/e2e/treevolution-config.cy.js
+++ cypress/e2e/treevolution-config.cy.js
@@ -9,32 +9,25 @@ afterEach(() => {
     })
 
 
-describe('Story 8', () => {
-        it(' le nombre d’espèces d’arbres séléctionnés est correct', () => {
-            cy.visit('http://0.0.0.0:5000/config')
-            cy.visit('http://0.0.0.0:5000/config')
-            cy.visit('http://0.0.0.0:5000/config')
-            cy.get('#inputStartDate').type('2022-12-13')
-            cy.get('#inputEndDate').type('2023-12-20')
-            cy.get('#inputKindsTree').select('treevolution.models.trees.oak.Ash')
-            cy.get('input[type=range]').as('range').invoke('val',5)
-            cy.get('@range').should('have.value', 5)
-   
-
-            cy.get('input[type=range]')
-            .invoke('val')
-            .then((val1) => {
-            cy.get('button[type="submit"]').click()
-            cy.visit('http://0.0.0.0:5000/simulation')
-            cy.get('#initSimulationBtn').click()
-
-            cy.get('#legendPanel > table > tbody > tr:nth-child(2) > td:nth-child(2)')
-            .invoke('text')
-            .should((text2) => {
-                expect(val1).eq(text2)
-            })
-            })
-            cy.wait(500)
-            })
-            })
-
+describe('Story 9', () => {
+        it('Lorsqu’une simulation est créée depuis l’onglet /simulation à partir d’une configuration. Celle-ci peut-être supprimée si la configuration vient à changer.', () => {
+        cy.visit('http://0.0.0.0:5000/config')
+        cy.get('#inputStartDate').type('2022-12-13')
+        cy.get('#inputEndDate').type('2023-12-20')
+        cy.get('#inputKindsTree').select('treevolution.models.trees.oak.Ash')
+        cy.get('input[type=range]').as('range').invoke('val',5)
+        cy.get('@range').should('have.value', 5)
+        cy.get('button[type="submit"]').click()
+        
+        cy.visit('http://0.0.0.0:5000/simulation')
+        cy.get('#initSimulationBtn').click()
+        cy.get('#runSimulationBtn').click()
+        cy.visit('http://0.0.0.0:5000/config')
+        cy.get('#inputStartDate').type('2022-12-15')
+        cy.get('button[type="submit"]').click()
+        cy.wait(500)
+        cy.visit('http://0.0.0.0:5000/simulation')
+        cy.get('#legendPanel > div > p > i').contains('No current simulation available')
+        
+        })
+})
