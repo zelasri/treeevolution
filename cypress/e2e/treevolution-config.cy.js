101c29aca738290d689c721621dbed540d04946a
zakaria.elasri2000 <zakaria.elasri2000@gmail.com>
2022-12-08 19:33:53 +0100

    story 11

--- cypress/e2e/treevolution-config.cy.js
+++ cypress/e2e/treevolution-config.cy.js
@@ -9,8 +9,8 @@ afterEach(() => {
     })
 
 
-describe('Story 10', () => {
-        it('verification que le nombre des arbres evolue a la fin de la simulation', () => {
+describe('Story 11', () => {
+        it('Lorsqu’une simulation est exécutée, puis mise en pause,la date et le nombre d’arbres sont restés fixes après un certain délai.', () => {
         cy.visit('http://0.0.0.0:5000/config')
         cy.get('#inputStartDate').type('2022-12-13')
         cy.get('#inputEndDate').type('2023-12-20')
@@ -20,16 +20,52 @@ describe('Story 10', () => {
         cy.get('button[type="submit"]').click()
         cy.visit('http://0.0.0.0:5000/simulation')
         cy.get('#initSimulationBtn').click()
+        cy.get('#runSimulationBtn').click()
+        cy.wait(200)
+//  verification que le nombre des arbres et le meme apres la pause
+
         cy.get('#legendPanel > table > tbody > tr:nth-child(2) > td:nth-child(2)')
         .invoke('text')
         .then((text1) => {
         cy.get('#runSimulationBtn').click()
-        cy.get('#simulationProgress',{timeout: 20000}).should('contain', '100%')
         cy.get('#legendPanel > table > tbody > tr:nth-child(2) > td:nth-child(2)')
         .invoke('text')
         .should((text2) => {
-            expect(text1).not.eq(text2)
-            })
-            })
-            })
-            })
+            expect(text1).eq(text2)
+        })
+        })
+
+        cy.get('#runSimulationBtn').click()
+        cy.wait(200)
+        cy.get('#runSimulationBtn').click()
+//  verification que la date legend et le meme apres la pause
+        cy.get('#legendTitle > div > div.col-md-9')
+        .invoke('text')
+        .then((text1) => {
+               var text1 = text1.split(" ");
+        cy.get('#legendTitle > div > div.col-md-9')
+        .invoke('text')
+        .should((text2) => {
+            var text2 = text2.split(" ");
+            expect(text1[1]).eq(text2[1])
+                })
+                })
+
+//  verification que le nombre des seeds et le meme apres la pause               
+
+        cy.get('#legendPanel > table > tbody > tr:nth-child(3) > td:nth-child(2)')
+        .invoke('text')
+        .then((text1) => {
+        cy.get('#legendPanel > table > tbody > tr:nth-child(3) > td:nth-child(2)')
+        .invoke('text')
+        .should((text2) => {
+        expect(text1).eq(text2)
+                 })
+                })
+           
+        })
+       
+        
+        
+        
+})
